acf446aa62f792ac6cce7827702fd8f7d05033e0
Valerii Lider <valerii.lider@gmail.com>
2021-02-18 19:23:44 +0100

    removed Envelope and Body

--- register_invoice_request.go
+++ register_invoice_request.go
@@ -5,42 +5,6 @@ import (
 	"time"
 )
 
-// RegisterInvoiceRequestEnvelope represents SOAP Envelope of RegisterInvoiceRequest
-type RegisterInvoiceRequestEnvelope struct {
-	XMLName xml.Name `xml:"http://schemas.xmlsoap.org/soap/envelope/ Envelope"`
-	Body    Body     `xml:"Body"`
-}
-
-// Body represents SOAP Envelope body
-type Body struct {
-	XSD                    XSD                    `xml:"xsd,attr"`
-	XSI                    XSI                    `xml:"xsi,attr"`
-	RegisterInvoiceRequest RegisterInvoiceRequest `xml:"RegisterInvoiceRequest"`
-}
-
-type XSD string
-type XSI string
-
-func (attr *XSD) MarshalXMLAttr(name xml.Name) (xml.Attr, error) {
-	return xml.Attr{
-		Name: xml.Name{
-			Space: "xsd",
-			Local: "xmlns",
-		},
-		Value: "http://www.w3.org/2001/XMLSchema",
-	}, nil
-}
-
-func (attr *XSI) MarshalXMLAttr(name xml.Name) (xml.Attr, error) {
-	return xml.Attr{
-		Name: xml.Name{
-			Space: "xsi",
-			Local: "xmlns",
-		},
-		Value: "http://www.w3.org/2001/XMLSchema-instance",
-	}, nil
-}
-
 // RegisterInvoiceRequest represents details neede for registering an invoice
 type RegisterInvoiceRequest struct {
 	XMLName   xml.Name  `xml:"https://efi.tax.gov.me/fs/schema RegisterInvoiceRequest"`
